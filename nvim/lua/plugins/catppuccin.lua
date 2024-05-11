e432a13f32324b28279d1b40109e019642c8ec08
D4lj337 <DALJEETSINGHC0@GMAIL.COM>
2024-05-11 09:31:54 +0000

    Add files via upload

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,20 @@
+return {
+require("catppuccin").setup {
+    color_overrides = {
+        all = {
+            text = "#ffffff",
+        },
+        latte = {
+            base = "#ff0000",
+            mantle = "#242424",
+            crust = "#474747",
+        },
+        frappe = {},
+        macchiato = {},
+        mocha = {
+            base = "#000000",
+            mantle = "#000000",
+            crust = "#000000",
+	
+},
+    }}}
